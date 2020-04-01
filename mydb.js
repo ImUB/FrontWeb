4098e5620b29a546f264b92ce357250f1a4e58b2
ImUB <dladbql1234@gmail.com>
2020-04-01 22:01:29 +0900

    web sql commit

--- mydb.js
+++ mydb.js
@@ -1,24 +1,41 @@
-var db;
-window.onload = function(){
-  if(!window.openDatabase) {
-    alert('Web SQL Database를 지원하지 않습니다.');
-    return;
-  }
-  db = openDatabase('mydb','1.0','회원정보 DB', 1024*1024);
+var id = document.getElementById('username2');
+var pass = document.getElementById('pass1');
+var name = document.getElementById('name');
+var sex = document.getElementsByName('sex');
+var tel = document.getElementById('tel');
+var mail = document.getElementById('mail');
+var birth = document.getElementById('birth');
+var age = document.getElementById('age');
+var hot = document.getElementById('hot');
+var db
 
-  db.transaction(function(tx) {
-    tx.executeSql('create table if not exists mydb(mydb_id integer primary key autoincrement,my_id, my_pass, my_name, my_sex, my_tel, my_mail, my_birth, my_age, my_hot)');
-  });
-
-  load();
-
-  document.getElementById('myForm').onsubmit = function(){
-
-    if(document.getElementById('btnadd').value == '확인'){
-      add();
-    }
-    else {
-      window.close;
-    }
-  }
+if (!window.openDatabase)
+{
+    alert("이 Browser 는 Web SQL Database 를 지원하지 않습니다.");
 }
+else
+{
+      db = openDatabase("MyDB", "1.0", "고객 DB", 2*1024*1024);
+      db.transaction(function(tx) {
+        tx.executeSql('CREATE TABLE IF NOT EXISTS mydb (id, pass, name, sex, tel, mail, birth, age, hot)');
+      });
+};
+
+function saveCustom() {
+  db.transaction(function(tx) {
+    tx.executeSql('')
+  });
+}
+
+// function insertData()
+// {
+
+//
+//   db.transaction(function(tx)
+//   {
+//     tx.executeSql("insert into custom values(?,?,?,?,?,?,?,?,?)",
+//     [id.value,pass.value,name.value,sex.value,tel.value,mail.value,birth.value,age.value,hot.value]);
+//   });
+//
+//   alert('데이터 추가 완료');
+// }
